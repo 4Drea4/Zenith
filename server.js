6221955b9482a17088655161ceeb6891fd227d8d
4drea4 <bakerand@outlook.com>
2026-01-18 15:44:10 -0600

    set up server files with basic expected routes and middleware

---
+++ server.js
@@ -0,0 +1,24 @@
+require ('dotenv').config();
+const express = require('express');
+const app = express (); //create an express app
+const connectDB = require('config/connection'); //database connection
+const productRoutes = require('/routes/productRoutes'); //routes
+const PORT = process.env.PORT || 3001;
+
+
+//Middleware
+app.use(express.json()) //req body for JSON
+app.use(express.urlencoded({extended:true})); //for form data
+
+//connect to database
+connectDB();
+
+//routes
+app.get('/', (req,res)=>{
+    res.send('<h1>Zenith API</h1>');
+})
+app.use('/api/products', productRoutes);
+//listen to the port
+app.listen(PORT, () =>{
+    console.log(`Server is running on the ${PORT}`)
+});
