1eb3b8611301730c535f18ed7586135ef41645d5
4drea4 <bakerand@outlook.com>
2026-01-18 16:23:18 -0600

    retrieve api for products id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -16,4 +16,21 @@ router.post('/', async (req,res)=>{
 }
 })
 
+//get api for products id to read a single product
+router.get('/:id',async (req,res)=> {
+    try{
+        const findProduct = await Product.findById(req.params.id);
+
+        if(!findProduct) {
+            return res.status(404).json({message:'Couldnt find the product you are looking for'});
+        }
+        res.json(findProduct);
+    } catch (error) {
+        res.status(400).json({
+            message: 'This doesnt appear to be the right product ID',
+            error: error.message
+        });
+    }
+});
+
 module.exports = router;
